d0e1bfb443234f624e14b515efd19eec49077813
simon <simon@faltum.com>
2020-10-21 12:51:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anymate",
-    version="0.0.4",
+    version="0.0.5",
     author="Anymate ApS",
     author_email="simon@anymate.io",
     description="Anymate SDK",
