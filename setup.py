fb21fe5a838703ea068947b85079e5c55fac884d
Simon <simon@anymate.io>
2020-11-18 13:07:51 +0100

    Bumped to version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anymate",
-    version="1.0.0  ",
+    version="1.0.0",
     author="Anymate ApS",
     author_email="simon@anymate.io",
     description="Anymate SDK",
