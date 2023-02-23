0c284a90b2f69d3b09e7f48477d65e2c7d55be81
Simon Faltum <simon@anymate.io>
2023-02-23 09:53:11 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anymate",
-    version="1.0.7",
+    version="1.0.8",
     author="Anymate ApS",
     author_email="simon@anymate.io",
     description="Anymate SDK",
