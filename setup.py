b0c264cd6d054d7e095ed282d939487b9572105a
Simon <simon@anymate.io>
2024-01-24 21:38:23 +0100

    More updates to models

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anymate",
-    version="1.0.12",
+    version="1.0.13",
     author="Anymate ApS",
     author_email="simon@anymate.io",
     description="Anymate SDK",
